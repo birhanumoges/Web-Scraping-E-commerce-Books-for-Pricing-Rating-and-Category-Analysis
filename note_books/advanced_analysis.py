745c77211ea1e4bbc31c6eaa0db169add91f82a1
birhanumoges <birhanumoges726@gmail.com>
2026-05-03 15:52:22 -0300

    create file advanced_analysis.py

---
+++ note_books/advanced_analysis.py
@@ -0,0 +1,10 @@
+"""
+Advanced Analysis Script for Book Price Tracker
+Calculates average price per rating and generates comprehensive report
+"""
+
+import mysql.connector
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+
