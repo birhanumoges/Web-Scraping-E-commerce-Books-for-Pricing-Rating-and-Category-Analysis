8352363b496ab447ec254ee73d34054e11d023a9
birhanumoges <birhanumoges726@gmail.com>
2026-05-03 15:03:05 -0300

    add sql codes for checking the created table

--- note_books/06_books.sql
+++ note_books/06_books.sql
@@ -1,1 +1,23 @@
+-- Use your database
+USE book_tracker;
 
+-- See all books inserted
+SELECT * FROM books;
+
+-- Count how many books you've scraped
+SELECT COUNT(*) as total_books FROM books;
+
+-- See the first 10 books
+SELECT id, title, price, rating FROM books LIMIT 10;
+
+-- Main requirement: Average price per rating
+SELECT 
+    rating,
+    COUNT(*) as number_of_books,
+    ROUND(AVG(price), 2) as average_price,
+    ROUND(MIN(price), 2) as cheapest_book,
+    ROUND(MAX(price), 2) as most_expensive
+FROM books_tracker.books
+WHERE rating IS NOT NULL
+GROUP BY rating
+ORDER BY rating DESC;
