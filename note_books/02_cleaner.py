263ebae7166568cecb453bedb5b9b520c2455c90
birhanumoges <birhanumoges726@gmail.com>
2026-05-03 14:20:25 -0300

    Add Data cleaning codes

--- note_books/02_cleaner.py
+++ note_books/02_cleaner.py
@@ -1,1 +1,15 @@
+def clean_price(price_text):
+    return float(price_text.replace('£', '').strip())
 
+def convert_rating(rating_class):
+    mapping = {
+        "One": 1,
+        "Two": 2,
+        "Three": 3,
+        "Four": 4,
+        "Five": 5
+    }
+    for key in mapping:
+        if key in rating_class:
+            return mapping[key]
+    return 0
